7cc905909c2b9b0aa9e623b7656b227d1c5e08fc
Ene <enetorufaeniekezimene@gmail.com>
2025-05-19 14:31:40 -0200

    Revert "add hello console.log to hello.js"
    This reverts commit 3e8fe68d32ff88ace32467df3c7f2eeb29e505ec.

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
 console.log('Hello, Git!');
+<<<<<<< HEAD
 console.log('Hello, GitHub!');
 console.log('revert!');
+=======
+console.log('Hello, GitHub!');
+>>>>>>> parent of 3e8fe68 (add hello console.log to hello.js)
